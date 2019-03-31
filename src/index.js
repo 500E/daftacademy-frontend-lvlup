2ea2a3a3fd7fc762f39753e96967761b9d7311e5
Kuba <kubamierzejewski94@gmail.com>
2019-04-01 00:50:59 +0200

    sass folders structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './styles/main.scss';
+
+const pageNav = document.querySelector('.page-navigation');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+hamburgerMenu.addEventListener('click', () => {
+  pageNav.classList.toggle('active');
+});
