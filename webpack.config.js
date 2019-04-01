0dab76dc69232915bcc3175ee750011728691904
Kuba <kubamierzejewski94@gmail.com>
2019-04-01 11:07:57 +0200

    added postcss

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,23 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  mode: 'development',
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/template.html'
-    })
+    }),
+    new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin()
   ],
   module: {
     rules: [
@@ -27,7 +33,12 @@ module.exports = {
       },
       {
         test: /\.s(a|c)ss$/,
-        use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+        use: [
+          isProduction ? MiniCssExtractPlugin.loader : { loader: 'style-loader', options: { sourceMap: true } },
+          { loader: 'css-loader', options: { sourceMap: isProduction } },
+          { loader: 'postcss-loader', options: { sourceMap: isProduction } },
+          { loader: 'sass-loader', options: { sourceMap: isProduction } }
+        ]
       },
       {
         test: /\.html$/,
